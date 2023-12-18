9bd1bb3e317b1dd83cc423ff049eac70a8e3792a
StevieOrtega <stevenvortega@gmail.com>
2023-12-18 14:10:21 -0800

    needs work: pin down the open/hide btn

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const plusArray = document.getElementsByClassName("faq-query");
+const plusArray = document.getElementsByClassName("faq-query")
+const faqAns = document.getElementsByClassName('faq-answer')
 
-console.log(moreBtn);
-plusArray.forEach((btn, index) => {
-  console.log(btn, index);
-});
+// Array.from(faqAns).forEach((faq, index) =>{
+// })
+
+// function openSesame() {
+  
+//   }
+
+// Array.from(plusArray).forEach((btn, index) => {
+//   btn.classList.add(index)
+//   btn.addEventListener("click", openSesame)
+// });
+
+
+// What am i going to do right now????? 
+// 1. i need to access the button of the div
+// 2. once the button is click then i need to hide/open the text box.
